76bd36c7277b8be1c65f828f1c086b2fb8cf52ea
unknown <rasyabayu857@gmail.com>
2025-01-16 14:28:23 +0700

    fix

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,11 @@
+import 
+
+export const createSlice = createSlice
+
+
+
+
+    addTocart: (state, payload) => {
+
+        const findProduct = state?.find((product)) => product.id === payload?.payload.
+    }
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from './productSlice'
+import cartReducer from './cartSlice'
+
+export default configureStore({
+    reducer: {
+        product: productReducer,
+        cart: cartReducer
+    }
+})
+
+
